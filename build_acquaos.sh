33d9954f56cde8276128c5c56774c8b88a71a100
FelidZero <giulianogramaglia69@gmail.com>
2026-05-01 19:47:12 +0200

    Update build_acquaos.sh

--- build_acquaos.sh
+++ build_acquaos.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+set -e
+echo "--- Inizio Build Reale AcquaOS Crystal ---"
 
-# 1. Crea le cartelle necessarie nella ISO
-mkdir -p build/etc/skel/.config/gtk-3.0
-mkdir -p build/etc/systemd/
-mkdir -p out
+# 1. Installazione strumenti necessari sul server
+sudo apt-get update
+sudo apt-get install -y arch-install-scripts erofs-utils units bsdtar
 
-# 2. Imposta il DARK MODE (Il look di AcquaOS)
+# 2. Setup della struttura Archiso
+mkdir -p releng
+cp -r /usr/share/archiso/configs/releng/* releng/ || true
+
+# 3. Inserimento della tua lista pacchetti
+cat packages.x86_64 >> releng/packages.x86_64
+
+# 4. Applicazione personalizzazioni (Dark Mode & Always On)
+mkdir -p releng/airootfs/etc/skel/.config/gtk-3.0
+mkdir -p releng/airootfs/etc/systemd/
+
 echo "[Settings]
 gtk-theme-name=Adwaita-dark
-gtk-application-prefer-dark-theme=1" > build/etc/skel/.config/gtk-3.0/settings.ini
+gtk-application-prefer-dark-theme=1" > releng/airootfs/etc/skel/.config/gtk-3.0/settings.ini
 
-# 3. Imposta l'ALWAYS ON (Niente sospensione)
-echo -e "[Login]\nHandleLidSwitch=ignore\nIdleAction=ignore" > build/etc/systemd/logind.conf
+echo -e "[Login]\nHandleLidSwitch=ignore\nIdleAction=ignore" > releng/airootfs/etc/systemd/logind.conf
 
-# 4. Crea l'identità di AcquaOS (Quello che leggi nel terminale)
-echo 'NAME="AcquaOS"
-ID=acquaos
-ID_LIKE=arch
-PRETTY_NAME="AcquaOS Crystal"
-ANSI_COLOR="0;34"' > build/etc/os-release
-
-echo "Configurazione AcquaOS completata con successo!"
+# 5. Generazione della ISO
+mkdir -p output
+sudo mkarchiso -v -w work_dir -o output releng/
